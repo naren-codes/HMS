cab7583e441e08c1c9c72ff197cfec9c48332762
naren-codes <narendhranath.mathialagan@cognizant.com>
2025-08-01 11:49:29 +0530

    demo dev

--- obj/Debug/net8.0/HMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/HMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33587c23dd5054ddc8453011b370ae8cf002ab41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3fcc787c39d0253ade2cb98d7e7ad118bcc8d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
